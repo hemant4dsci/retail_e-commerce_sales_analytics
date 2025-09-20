c58075d17f38f1e929ff25e27f089a8d565f30a1
hemant4dsci <hemant4dsci@gmail.com>
2025-09-20 13:44:27 +0530

    sql script to create db

--- sql/create_db_load_data.sql
+++ sql/create_db_load_data.sql
@@ -1,3 +1,5 @@
+-- Execute below queries step by step to create database and then table.
+
 -- Creating database
 CREATE DATABASE IF NOT EXISTS walmart_sales;
 
@@ -19,8 +21,102 @@ CREATE TABLE calender (
 );
 
 -- Insert data in calender table
-LOAD DATA INFILE 'E:/Data Sets/E-Commerce Vendor Data/calender.csv'
-INTO TABLE brands
+LOAD DATA INFILE '../data/raw/calender.csv'
+INTO TABLE calender
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Creating channel table
+CREATE TABLE channels (
+    channel_key INTEGER PRIMARY KEY,
+    channel_name VARCHAR(255)
+);
+
+-- Insert data in channel table
+LOAD DATA INFILE '../data/raw/channel.csv'
+INTO TABLE channels
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Creating geography table
+CREATE TABLE geography (
+    geo_key INTEGER PRIMARY KEY,
+    continent_name VARCHAR(255),
+    country_name VARCHAR(255),
+    state_province_name VARCHAR(255),
+    city_name VARCHAR(255)
+);
+
+-- Insert data in geography table
+LOAD DATA INFILE '../data/raw/geography.csv'
+INTO TABLE geography
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Creating product sub category table
+CREATE TABLE product_sub_category (
+    product_sub_category_key INTEGER PRIMARY KEY,
+    product_sub_category_name VARCHAR(255),
+    product_category_name VARCHAR(255)
+);
+
+-- Insert data in geography table
+LOAD DATA INFILE '../data/raw/product_sub_category.csv'
+INTO TABLE product_sub_category
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Creating products table
+CREATE TABLE products (
+    product_key INTEGER PRIMARY KEY,
+    product_name VARCHAR(255),
+    brand_name VARCHAR(255),
+    class_name VARCHAR(255),
+    color_name VARCHAR(255)
+);
+
+-- Insert data in geography table
+LOAD DATA INFILE '../data/raw/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Creating sales table
+CREATE TABLE sales (
+    sales_key INTEGER PRIMARY KEY,
+    dates DATE,
+    channel_key INTEGER,
+    product_key INTEGER,
+    promotion_key INTEGER,
+    geo_key INTEGER,
+    product_sub_category_key INTEGER,
+    unit_cost DECIMAL(10,2),
+    unit_price DECIMAL(10,2),
+    sales_quantity INTEGER,
+    return_quantity INTEGER,
+    return_amount DECIMAL(10,2),
+    discount_quantity INTEGER,
+    discount_amount DECIMAL(10,2)
+);
+
+-- Insert data in geography table
+LOAD DATA INFILE '../data/raw/sales.csv'
+INTO TABLE sales
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
