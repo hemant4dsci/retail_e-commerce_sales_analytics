f3e394ab576475514d3c7ccb3a6515efef9ced99
hemant4dsci <hemant4dsci@gmail.com>
2025-10-15 14:00:57 +0530

    db change mysql to postgresql

--- sql_scripts/create_db_load_data.sql
+++ sql_scripts/create_db_load_data.sql
@@ -1,136 +1,115 @@
 -- Execute below queries step by step to create database and then table.
-
--- Creating database
-
-CREATE DATABASE IF NOT EXISTS walmart_sales;
-
--- Choosing the database
-
-USE walmart_sales;
-
 -- Creating calender table
-
 CREATE TABLE calender (
-    dates DATE PRIMARY KEY,
-    years YEAR,
+    date_key DATE,
+    years SMALLINT,
     half_year CHAR(2),
     quarters CHAR(2),
-    months VARCHAR(255),
-    weeks VARCHAR(255),
-    is_work_day VARCHAR(255),
-    europe_session VARCHAR(255),
-    north_america_session VARCHAR(255),
-    asia_session VARCHAR(255)
+    months VARCHAR(10),
+    weeks VARCHAR(10),
+    is_work_day VARCHAR(10),
+    europe_session VARCHAR(20),
+    north_america_session VARCHAR(30),
+    asia_session VARCHAR(30),
+    CONSTRAINT pk_date_key PRIMARY KEY (date_key)
 );
 
 -- Insert data in calender table
-
-LOAD DATA INFILE '../data/raw/calender.csv' -- path of your file
-INTO TABLE calender
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
+COPY calender
+FROM
+    'E:/Data Sets/costoso_data_iim/calender.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 -- Creating channel table
-
 CREATE TABLE channels (
-    channel_key INTEGER PRIMARY KEY,
-    channel_name VARCHAR(255)
+    channel_key SMALLINT,
+    channel_name VARCHAR(10),
+    CONSTRAINT pk_channel_key PRIMARY KEY (channel_key)
 );
 
 -- Insert data in channel table
-
-LOAD DATA INFILE '../data/raw/channel.csv' -- path of your file
-INTO TABLE channels
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
+COPY channels
+FROM
+    'E:/Data Sets/costoso_data_iim/channel.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 -- Creating geography table
-
-CREATE TABLE geography (
-    geo_key INTEGER PRIMARY KEY,
-    continent_name VARCHAR(255),
-    country_name VARCHAR(255),
-    state_province_name VARCHAR(255),
-    city_name VARCHAR(255)
+CREATE TABLE geographies (
+    geo_key SMALLINT,
+    continent_name VARCHAR(20),
+    city_name VARCHAR(25),
+    state_province_name VARCHAR(40),
+    country_name VARCHAR(25),
+    CONSTRAINT pk_geo_key PRIMARY KEY (geo_Key)
 );
 
 -- Insert data in geography table
+COPY geographies
+FROM
+    'E:/Data Sets/costoso_data_iim/geography.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
-LOAD DATA INFILE '../data/raw/geography.csv' -- path of your file
-INTO TABLE geography
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
-
--- Creating product sub category table
+-- Creating product sub-category table
 CREATE TABLE product_sub_category (
-    product_sub_category_key INTEGER PRIMARY KEY,
-    product_sub_category_name VARCHAR(255),
-    product_category_name VARCHAR(255)
+    product_sub_category_key SMALLINT,
+    product_sub_category_name VARCHAR(40),
+    product_category_name VARCHAR(40),
+    CONSTRAINT pk_product_sub_category_key PRIMARY KEY (product_sub_category_key)
 );
 
--- Insert data in geography table
-LOAD DATA INFILE '../data/raw/product_sub_category.csv' -- path of your file
-INTO TABLE product_sub_category
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
+-- Insert data in product sub-category table
+COPY product_sub_category
+FROM
+    'E:/Data Sets/costoso_data_iim/product_sub_category.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 -- Creating products table
 CREATE TABLE products (
-    product_key INTEGER PRIMARY KEY,
-    product_name VARCHAR(255),
-    brand_name VARCHAR(255),
-    class_name VARCHAR(255),
-    color_name VARCHAR(255)
+    product_key SMALLINT,
+    product_name TEXT,
+    brand_name VARCHAR(25),
+    class_name VARCHAR(15),
+    color_name VARCHAR(15),
+    CONSTRAINT pk_product_key PRIMARY KEY (product_key)
 );
 
--- Insert data in geography table
-LOAD DATA INFILE '../data/raw/products.csv' -- path of your file
-INTO TABLE products
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
+-- Insert data in products table
+COPY products
+FROM
+    'E:/Data Sets/costoso_data_iim/products.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 -- Creating sales table
 CREATE TABLE sales (
-    sales_key INTEGER PRIMARY KEY,
-    dates DATE,
-    channel_key INTEGER,
-    product_key INTEGER,
-    promotion_key INTEGER,
-    geo_key INTEGER,
-    product_sub_category_key INTEGER,
-    unit_cost DECIMAL(10,2),
-    unit_price DECIMAL(10,2),
-    sales_quantity INTEGER,
-    return_quantity INTEGER,
-    return_amount DECIMAL(10,2),
-    discount_quantity INTEGER,
-    discount_amount DECIMAL(10,2)
+    sales_key INTEGER,
+    date_key DATE,
+    channel_key SMALLINT,
+    product_key SMALLINT,
+    geo_key SMALLINT,
+    product_sub_category_key SMALLINT,
+    unit_cost NUMERIC(7, 2),
+    unit_price NUMERIC(7, 2),
+    sales_quantity SMALLINT,
+    return_quantity SMALLINT,
+    return_amount NUMERIC(7, 2),
+    discount_quantity SMALLINT,
+    discount_amount NUMERIC(7, 2),
+    CONSTRAINT pk_sales_key PRIMARY KEY (sales_key),
+    CONSTRAINT fk_date_key FOREIGN KEY (date_key) REFERENCES calender (date_key),
+    CONSTRAINT fk_channel_key FOREIGN KEY (channel_key) REFERENCES channels (channel_key),
+    CONSTRAINT fk_product_key FOREIGN KEY (product_key) REFERENCES products (product_key),
+    CONSTRAINT fk_geo_key FOREIGN KEY (geo_key) REFERENCES geographies (geo_key),
+    CONSTRAINT fk_product_sub_category FOREIGN KEY (product_sub_category_key) REFERENCES product_sub_category (product_sub_category_key)
 );
 
--- Insert data in geography table
-LOAD DATA INFILE '../data/raw/sales.csv' -- path of your file
-INTO TABLE sales
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- Insert data in sales table
+COPY sales
+FROM
+    'E:/Data Sets/costoso_data_iim/sales.csv'
+WITH
+    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
